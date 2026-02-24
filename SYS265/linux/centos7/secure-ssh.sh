6962473626aad74f28723a8a24d909a8e828e178
lizzy9596 <elizabeth.mayo@mymail.champlain.edu>
2026-02-24 14:16:46 -0500

    centos7/secure-ssh.sh

---
+++ SYS265/linux/centos7/secure-ssh.sh
@@ -0,0 +1,6 @@
+#secure-ssh.sh
+#author lizzy9596
+#creates a new ssh user using $1 parameter
+#adds a public key from the local repo or curled from the remote repo
+#removes root apility to ssh in
+echo "ALL YOUR CODE GOES HERE" 
